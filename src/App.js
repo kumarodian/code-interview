c023ffa9bcfed5c93b796943485c8d765d8121e1
kumarodian <sachinyadav607@gmail.com>
2022-08-07 13:44:13 +0530

    Fetch data, sort, sum up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [data, setData] = React.useState([]);
+  let totalCost = 0;
+  React.useEffect(() => {
+    for (let i = 1; i <= 3; i++)
+      fetch(`/branch${i}.json`)
+        .then((res) => res.json())
+        .then((data) => {
+          const storedata = data.products;
+          setData((prev) => [...storedata, ...prev]);
+        });
+  }, []);
+  //sort alphabetically
+  data.sort((a, b) => {
+    let na = a.name.toLowerCase(),
+      nb = b.name.toLowerCase();
+    if (na < nb) return -1;
+    if (na > nb) return 1;
+    return 0;
+  });
+
+  let sumUpdata = data.reduce((b, a) => {
+    let ind = b.findIndex((e) => e.name === a.name);
+    let c = a.name.toLowerCase().split(" ").join("");
+    if (ind > -1) {
+      if (!b[ind].total) b[ind].total = 0;
+      b[ind].total += a.unitPrice * a.sold;
+    } else {
+      a[c] = +a[c] || 0;
+      if (!a.total) a.total = 0;
+      a.total += a.unitPrice * a.sold;
+      b.push(a);
+    }
+    return b;
+  }, []);
+  const store = (
+    <table>
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th>Total Revenue</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sumUpdata.map(({ id, name, total }) => {
+          totalCost += total;
+          return (
+            <tr key={id + name}>
+              <td>{name}</td>
+              <td>{new Intl.NumberFormat("en-IN").format(total)}</td>
+            </tr>
+          );
+        })}
+        <tr>
+          <th>Total</th>
+          <th>{new Intl.NumberFormat("en-IN").format(totalCost)}</th>
+        </tr>
+      </tbody>
+    </table>
   );
+  return <div>{store}</div>;
 }
 
 export default App;
